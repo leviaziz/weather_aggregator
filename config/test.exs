c998d6b570bd14c6945f809350ff4bcc4cbfb38c
furqanaziz14 <furqan.dev14@invomail.io>
2023-09-15 23:00:36 +0500

    mix format

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :weather_aggregator, WeatherAggregatorWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :weather_aggregator, WeatherAggregator.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :weather_aggregator, WeatherAggregator.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
--- test/weather_aggregator_web/controllers/error_json_test.exs
+++ test/weather_aggregator_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule WeatherAggregatorWeb.ErrorJSONTest do
   use WeatherAggregatorWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert WeatherAggregatorWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert WeatherAggregatorWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
